3e77bf70f0849d66e36e564f7c7750982e7e4960
Andreas Elia <andreaselia@live.co.uk>
2020-11-07 17:23:59 +0000

    desc

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,11 +12,11 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'not a number',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: 'Why fit in when you can stand out.' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: 'data:image/x-icon;,' }
