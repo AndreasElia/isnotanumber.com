a4eb91364b7ccf33486803344dbb99af5917e0b1
Andreas Elia <andreaselia@live.co.uk>
2020-11-07 17:30:59 +0000

    fix

--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,28 @@
 <template>
-  <div class="h-screen flex flex-col justify-between">
-    <div class="container mx-auto h-full">
-      <div class="h-full pt-8 px-4 sm:pt-12 sm:px-6 md:pt-20 xl:pt-24">
-        <h1 class="text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl">
-          We are not a number.
-        </h1>
-        <h2 class="mt-4 text-4xl tracking-tight leading-10 font-extrabold text-pink-500 sm:text-5xl sm:leading-none md:text-6xl">
-          Why fit in when
-          <br class="xl:hidden">
-          <span class="text-pink-700">you can stand out.</span>
-        </h2>
-      </div>
-      <div class="pb-8 px-4 text-xs text-gray-700 sm:pb-12 sm:px-6 md:pb-20 xl:pb-24">
-        <div class="flex justify-between items-center">
-          <p>
-            Company registered in England No: <a href="https://beta.companieshouse.gov.uk/company/12813978" target="_blank">12813978</a><br>
-            The Garden Cottage, Walnut Tree Farm, Corse Lawn,
-            <br class="md:hidden">
-            Gloucester, United Kingdom, GL19 4ND
-          </p>
-          <a href="https://www.linkedin.com/company/isnotanumber" target="_blank">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" /></svg>
-          </a>
-        </div>
+  <div class="container mx-auto flex flex-col h-screen justify-between">
+    <div class="mt-8 px-4 sm:mt-12 sm:px-6 md:mt-20 xl:mt-24">
+      <h1 class="text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl">
+        We are not a number.
+      </h1>
+      <h2 class="mt-4 text-4xl tracking-tight leading-10 font-extrabold text-pink-500 sm:text-5xl sm:leading-none md:text-6xl">
+        Why fit in when
+        <br class="xl:hidden">
+        <span class="text-pink-700">you can stand out.</span>
+      </h2>
+    </div>
+    <div class="mb-8 px-4 text-xs text-gray-700 sm:mb-12 sm:px-6 md:mb-20 xl:mb-24">
+      <div class="flex justify-between items-center">
+        <p>
+          Company registered in England No: <a href="https://beta.companieshouse.gov.uk/company/12813978" target="_blank">12813978</a><br>
+          The Garden Cottage, Walnut Tree Farm, Corse Lawn,
+          <br class="md:hidden">
+          Gloucester, United Kingdom, GL19 4ND
+        </p>
+        <a href="https://www.linkedin.com/company/isnotanumber" target="_blank">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
+            <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
+          </svg>
+        </a>
       </div>
     </div>
   </div>
