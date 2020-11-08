a6ffef3955255c26149cc9682327024edff2d0d4
Andreas Elia <andreaselia@live.co.uk>
2020-11-08 21:41:27 +0000

    test

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="my-6 container mx-auto sm:my-12">
-    <TheHeader />
+    <the-header />
 
     <h2
       class="mt-16 lg:mt-32 text-2xl tracking-loose leading-10 font-bold sm:text-5xl sm:leading-none md:text-6xl"
