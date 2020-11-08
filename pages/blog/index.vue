0ef442ce8e2dde6ea86786389def61e07e7ee41d
Andreas Elia <andreaselia@live.co.uk>
2020-11-08 20:49:22 +0000

    blog

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -10,28 +10,30 @@
       Articles
     </h2>
 
-    <div class="mt-16 md:mt-32 lg:mt-40 grid grid-cols-1 md:grid-cols-2 gap-x-8 gap-y-12">
-      <NuxtLink
-        v-for="(article, index) of articles"
-        :key="article.slug"
-        data-aos="fade-up"
-        :data-aos-delay="(200 * index) + 400"
-        :to="{ name: 'blog-slug', params: { slug: article.slug } }"
-      >
-        <div class="transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110">
-          <div
-            class="w-full h-64 bg-gray-400 rounded-md bg-cover bg-center"
-            :style="{ backgroundImage: `url(${article.img})` }"
-          ></div>
-          <h3 class="mt-1">
-            {{ article.title }}
-          </h3>
-          <p class="text-gray-200 text-sm">
-            {{ article.description }}
-          </p>
-        </div>
-      </NuxtLink>
-    </div>
+    <client-only>
+      <div class="mt-16 md:mt-32 lg:mt-40 grid grid-cols-1 md:grid-cols-2 gap-x-8 gap-y-12">
+        <NuxtLink
+          v-for="(article, index) of articles"
+          :key="article.slug"
+          data-aos="fade-up"
+          :data-aos-delay="(200 * index) + 400"
+          :to="{ name: 'blog-slug', params: { slug: article.slug } }"
+        >
+          <div class="transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110">
+            <div
+              class="w-full h-64 bg-gray-400 rounded-md bg-cover bg-center"
+              :style="{ backgroundImage: `url(${article.img})` }"
+            ></div>
+            <h3 class="mt-1">
+              {{ article.title }}
+            </h3>
+            <p class="text-gray-200 text-sm">
+              {{ article.description }}
+            </p>
+          </div>
+        </NuxtLink>
+      </div>
+    </client-only>
 
     <all-the-same />
 
