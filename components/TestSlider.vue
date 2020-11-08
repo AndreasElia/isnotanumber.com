4bb0d609e0f93f05c917c028635cfb1a906f1673
Andreas Elia <andreaselia@live.co.uk>
2020-11-08 22:53:16 +0000

    slider

---
+++ components/TestSlider.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <VueSlickCarousel :arrows="true" :dots="true">
+      <div>1</div>
+      <div>2</div>
+      <div>3</div>
+      <div>4</div>
+    </VueSlickCarousel>
+  </div>
+</template>
+
+<script>
+import VueSlickCarousel from 'vue-slick-carousel'
+import 'vue-slick-carousel/dist/vue-slick-carousel.css'
+import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css'
+
+export default {
+  components: { VueSlickCarousel }
+}
+</script>
