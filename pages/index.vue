60aed609377fb586ffadb6efd1aefe0581d6d4c7
Andreas Elia <andreaselia@live.co.uk>
2020-11-08 21:20:38 +0000

    fix

--- pages/index.vue
+++ pages/index.vue
@@ -15,42 +15,50 @@
     </h2>
 
     <div class="mt-16 md:mt-32 lg:mt-40 grid grid-cols-1 md:grid-cols-2 gap-x-8 gap-y-12">
-      <div data-aos="fade-up" data-aos-delay="400">
-        <div
-          class="w-full h-64 bg-gray-400 rounded-md bg-cover bg-center"
-          style="background-image: url(https://images.unsplash.com/photo-1493612276216-ee3925520721?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60);"
-        ></div>
-        <h3 class="mt-1">
-          balansa+
-        </h3>
-      </div>
-      <div data-aos="fade-up" data-aos-delay="600">
-        <div
-          class="w-full h-64 bg-gray-400 rounded-md bg-cover bg-center"
-          style="background-image: url(https://images.unsplash.com/photo-1493612276216-ee3925520721?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60);"
-        ></div>
-        <h3 class="mt-1">
-          My Go Hire
-        </h3>
-      </div>
-      <div data-aos="fade-up" data-aos-delay="800">
-        <div
-          class="w-full h-64 bg-gray-400 rounded-md bg-cover bg-center"
-          style="background-image: url(https://images.unsplash.com/photo-1493612276216-ee3925520721?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60);"
-        ></div>
-        <h3 class="mt-1">
-          GradSmart
-        </h3>
-      </div>
-      <div data-aos="fade-up" data-aos-delay="1000">
-        <div
-          class="w-full h-64 bg-gray-400 rounded-md bg-cover bg-center"
-          style="background-image: url(https://images.unsplash.com/photo-1493612276216-ee3925520721?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60);"
-        ></div>
-        <h3 class="mt-1">
-          Craftr
-        </h3>
-      </div>
+      <NuxtLink data-aos="fade-up" data-aos-delay="400">
+        <div class="transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110">
+          <div
+            class="w-full h-64 bg-gray-400 rounded-md bg-cover bg-center"
+            style="background-image: url(https://images.unsplash.com/photo-1493612276216-ee3925520721?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60);"
+          ></div>
+          <h3 class="mt-1">
+            balansa+
+          </h3>
+        </div>
+      </NuxtLink>
+      <NuxtLink data-aos="fade-up" data-aos-delay="600">
+        <div class="transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110">
+          <div
+            class="w-full h-64 bg-gray-400 rounded-md bg-cover bg-center"
+            style="background-image: url(https://images.unsplash.com/photo-1493612276216-ee3925520721?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60);"
+          ></div>
+          <h3 class="mt-1">
+            My Go Hire
+          </h3>
+        </div>
+      </NuxtLink>
+      <NuxtLink data-aos="fade-up" data-aos-delay="800">
+        <div class="transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110">
+          <div
+            class="w-full h-64 bg-gray-400 rounded-md bg-cover bg-center"
+            style="background-image: url(https://images.unsplash.com/photo-1493612276216-ee3925520721?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60);"
+          ></div>
+          <h3 class="mt-1">
+            GradSmart
+          </h3>
+        </div>
+      </NuxtLink>
+      <NuxtLink data-aos="fade-up" data-aos-delay="400">
+        <div class="transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110">
+          <div
+            class="w-full h-64 bg-gray-400 rounded-md bg-cover bg-center"
+            style="background-image: url(https://images.unsplash.com/photo-1493612276216-ee3925520721?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60);"
+          ></div>
+          <h3 class="mt-1">
+            Craftr
+          </h3>
+        </div>
+      </NuxtLink>
     </div>
 
     <all-the-same />
