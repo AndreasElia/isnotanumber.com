9daac42b29b89cd973cc9f38896c1e1f7a140619
Andreas Elia <andreaselia@live.co.uk>
2020-11-10 17:06:31 +0000

    nan

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex justify-between items-center" data-aos="fade-up">
+  <div class="flex items-center justify-between" data-aos="fade-up">
     <a href="/">
       <h1 class="font-bold text-white sm:leading-none md:text-lg">
         not a
@@ -8,18 +8,18 @@
       </h1>
     </a>
 
-    <button
+    <!-- <button
       @click="open = !open"
-      class="inline-flex items-center justify-center p-2 text-white hover:text-opacity-75 focus:outline-none focus:text-opacity-75 transition duration-150 ease-in-out"
+      class="inline-flex items-center justify-center p-2 text-white transition duration-150 ease-in-out hover:text-opacity-75 focus:outline-none focus:text-opacity-75"
     >
-      <svg :class="{ 'hidden': open, 'block': !open }" class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+      <svg :class="{ 'hidden': open, 'block': !open }" class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
       </svg>
 
-      <svg :class="{ 'hidden': !open, 'block': open }" class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+      <svg :class="{ 'hidden': !open, 'block': open }" class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
       </svg>
-    </button>
+    </button> -->
   </div>
 </template>
 
