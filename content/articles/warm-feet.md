7a07cc18ee2eba02b3ef8e82384cb2a9dbd74819
Andreas Elia <andreaselia@live.co.uk>
2021-01-11 12:03:09 +0000

    update

--- content/articles/warm-feet.md
+++
@@ -1,8 +0,0 @@
----
-title: Warm Feet
-description: That feeling when you're inside when it rains.
-img: https://images.unsplash.com/photo-1508881136857-d1781947f4d6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60
-alt: warm feet
----
-
-That warm fuzzy feeling, the one you get when you're ever so comfortable. It's raining and cold outside, but you're inside with your warm fluffy socks, the fire is burning away in the background whilst you're driving your favourite beverage and watching a nostalgic movie.
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,82 +1,13 @@
-/*
-** TailwindCSS Configuration File
-**
-** Docs: https://tailwindcss.com/docs/configuration
-** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
-*/
 module.exports = {
-  theme: {
-    typography: {
-      default: {
-        css: {
-          'h2, h3, h4': {
-            color: '#fff',
-          },
-        },
-      },
-    },
-    screens: {
-      sm: '640px',
-      md: '768px',
-      lg: '1024px',
-    },
-    extend: {
-      colors: {
-        gray: {
-          50: '#F4F4F4',
-          100: '#EAEAEA',
-          200: '#CACACA',
-          300: '#AAAAAA',
-          400: '#6B6B6B',
-          500: '#2B2B2B',
-          600: '#272727',
-          700: '#1A1A1A',
-          800: '#131313',
-          900: '#0D0D0D',
-        },
-        blue: {
-          50: '#F5F5FF',
-          100: '#EAEBFF',
-          200: '#CBCDFF',
-          300: '#ACAFFF',
-          400: '#6D72FF',
-          500: '#2F36FF',
-          600: '#2A31E6',
-          700: '#1C2099',
-          800: '#151873',
-          900: '#0E104D',
-        },
-      },
-    },
-  },
-  variants: {},
-  plugins: [
-    require('@tailwindcss/typography'),
+  purge: [
+    './public/index.html',
   ],
-  purge: {
-    // enabled: process.env.NODE_ENV === 'production',
-    enabled: true,
-    content: [
-      'components/**/*.vue',
-      'layouts/**/*.vue',
-      'pages/**/*.vue',
-      'plugins/**/*.js',
-      'nuxt.config.js',
-    ],
-    options: {
-      safelist: [
-        'h1',
-        'h2',
-        'h3',
-        'p',
-        'blockquote',
-        'strong',
-        'font-sans',
-        'antialiased',
-        'text-white',
-        'bg-gray-500',
-      ],
-      safelistPatterns: [/^aos-/],
-    }
-  }
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
 }
